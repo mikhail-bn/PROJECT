7c5aa4e811d46813ed7698c5294486801b44c424
mikhail-bn <mihail.news@gmail.com>
2023-01-18 20:49:45 +0200

    18-01-23-1

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,4 +1,5 @@
 // Подключение функционала "Чертогов Фрилансера"
+import { Navigation } from "swiper";
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
@@ -15,3 +16,33 @@ window.addEventListener("load", function(e){
     }
     
 });
+
+
+// Команда на запуск видео при его достижении скроллом (вотчером) по значению video селектора data-watch 
+
+if (document.querySelector('.video-module')) {
+    document.addEventListener("watcherCallback", function (e) {
+        const entry = e.detail.entry;
+        const targetElement = entry.target;
+        if(targetElement.dataset.watch === 'video') {
+            if(entry.isIntersecting) {
+                
+                targetElement.querySelector('video').play();
+            }
+            else {
+                targetElement.querySelector('video').pause();
+            }
+        }
+    });
+    document.querySelector('.video-module').addEventListener('click', function(e) {
+        if (!document.querySelector('.video-module').classList.contains('.init')) {
+        document.querySelector('.video-module').querySelector('video').src = document.querySelector('video').dataset.full;
+        document.querySelector('.video-module').querySelector('video').classList.add('_active');
+        document.querySelector('.video-module').querySelector('video').classList.add('_init');
+        document.querySelector('.video-module').querySelector('video').play();
+        document.querySelector('.video-module').querySelector('video').muted = false;
+    } else {
+        document.querySelector('.video-module').querySelector('video').pause();
+    }
+    });
+}
